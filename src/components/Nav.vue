e00d14b343d3521147ef45feecc58cb6bcbbf259
Cassandra <umehcassandra@gmail.com>
2023-03-12 13:59:07 +0100

    build github-app

---
+++ src/components/Nav.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <ul>
+      <li>Pull requests</li>
+      <li>About</li>
+      <li>Issues</li>
+      <li>Codespaces</li>
+      <li>Marketplace</li>
+      <li>Explore</li>
+    </ul>
+  </div>
+</template>
